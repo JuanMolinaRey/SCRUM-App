9c131502679db447e4ad456e7d440e7e933af28f
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-11 15:33:38 +0200

    feat: add methods for controllers

--- src/main/java/com/SCRUM/APP/controller/TaskController.java
+++ src/main/java/com/SCRUM/APP/controller/TaskController.java
@@ -1,4 +1,78 @@
 package com.SCRUM.APP.controller;
 
+import com.SCRUM.APP.dtos.task.TaskConverter;
+import com.SCRUM.APP.dtos.task.TaskDTO;
+import com.SCRUM.APP.model.Task;
+import com.SCRUM.APP.service.TaskService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("api/v1/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+    private final TaskConverter taskConverter;
+
+    public TaskController(TaskService taskService, TaskConverter taskConverter) {
+        this.taskService = taskService;
+        this.taskConverter = taskConverter;
+    }
+
+    @PostMapping
+    public TaskDTO createTask(@RequestBody TaskDTO taskDTO) {
+        Task task = taskConverter.dtoToTask(taskDTO);
+        Task createdTask = taskService.createTask(task);
+        return taskConverter.taskToDto(createdTask);
+    }
+
+    @GetMapping
+    public List<TaskDTO> getAllTasks() {
+        List<Task> tasks = taskService.getAllTasks();
+        return tasks.stream()
+                .map(taskConverter::taskToDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/task/{id}")
+    public ResponseEntity<TaskDTO> getTaskById(@PathVariable Long id) {
+        Optional<Task> taskOptional = taskService.getTaskById(id);
+
+        if (taskOptional.isPresent()) {
+            TaskDTO taskDTO = taskConverter.taskToDto(taskOptional.get());
+            return ResponseEntity.ok(taskDTO);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/task/{id}")
+    public ResponseEntity<TaskDTO> updateTask(@PathVariable Long id, @RequestBody TaskDTO taskDTO) {
+        Task task = taskConverter.dtoToTask(taskDTO);
+        task.setId(id);
+        Task updatedTask = taskService.updateTask(task);
+        if (updatedTask != null) {
+            TaskDTO updatedTaskDTO = taskConverter.taskToDto(updatedTask);
+            return ResponseEntity.ok(updatedTaskDTO);
+
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @DeleteMapping("/task/{id}")
+    public ResponseEntity<Void> deleteTaskById(@PathVariable Long id){
+        taskService.deleteTaskById(id);
+        return ResponseEntity.noContent().build();
+    }
+    @DeleteMapping
+    public ResponseEntity<Void> deleteAllCourses(){
+        taskService.deleteAllTasks();
+        return ResponseEntity.noContent().build();
+    }
 }
