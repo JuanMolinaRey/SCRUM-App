c76e424f32ffd459b91d14a87f6c09e7e9c44c7e
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-11 15:33:07 +0200

    feat: change setters and getters of id

--- src/main/java/com/SCRUM/APP/dtos/task/TaskDTO.java
+++ src/main/java/com/SCRUM/APP/dtos/task/TaskDTO.java
@@ -1,2 +1,73 @@
-package com.SCRUM.APP.dtos.task;public class taskDto {
+package com.SCRUM.APP.dtos.task;
+
+import com.SCRUM.APP.model.Project;
+import com.SCRUM.APP.model.User;
+
+
+public class TaskDTO {
+
+        private int id;
+        private String name;
+        private String description;
+        private boolean completed;
+        private Project project;
+        private User user;
+    public TaskDTO() {
+    }
+    public TaskDTO(int id, String name, String description, boolean completed, Project project, User user) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.completed = completed;
+        this.project = project;
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
