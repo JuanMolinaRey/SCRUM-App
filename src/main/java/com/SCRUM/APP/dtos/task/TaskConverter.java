972de2ebe7cb51fc71f1884b364558ae117eb8af
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-11 15:34:32 +0200

    fix: add constructor for model mapper to inject the dependency

--- src/main/java/com/SCRUM/APP/dtos/task/TaskConverter.java
+++ src/main/java/com/SCRUM/APP/dtos/task/TaskConverter.java
@@ -1,2 +1,22 @@
-package com.SCRUM.APP.dtos.task;public class taskConverter {
+package com.SCRUM.APP.dtos.task;
+
+
+import com.SCRUM.APP.model.Task;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskConverter {
+
+    private final ModelMapper modelMapper;
+
+    public TaskConverter(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+    public TaskDTO taskToDto (Task task){
+        return modelMapper.map (task, TaskDTO.class);
+    }
+    public Task dtoToTask (TaskDTO taskDTO){
+        return modelMapper.map (taskDTO, Task.class);
+    }
 }
