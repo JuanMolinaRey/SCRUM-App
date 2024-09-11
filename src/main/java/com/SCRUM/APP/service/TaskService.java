8ee45efcf6e66d310ce52a151bf7286d899896c8
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-11 15:35:40 +0200

    feat: add methods for task controllers

--- src/main/java/com/SCRUM/APP/service/TaskService.java
+++ src/main/java/com/SCRUM/APP/service/TaskService.java
@@ -1,4 +1,54 @@
 package com.SCRUM.APP.service;
 
+import com.SCRUM.APP.model.Task;
+import com.SCRUM.APP.repository.ITaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TaskService {
+
+    private final ITaskRepository taskRepository;
+
+    public TaskService(ITaskRepository taskRepository){
+        this.taskRepository = taskRepository;
+    }
+
+    public Task createTask(Task task){
+        return taskRepository.save(task);
+    }
+    public List<Task> getAllTasks(){
+        try {
+            return taskRepository.findAll();
+        } catch (Exception e) {
+            throw new RuntimeException("Error retrieving all users")
+        }
+    }
+    public Optional<Task> getTaskById (Long id){
+        try {
+            return taskRepository.findById(id);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error retrieving user by id");
+        }
+    }
+    public Task updateTask (Task updatedTask){
+        Optional<Task> existingTask = taskRepository.findById(updatedTask.getId());
+        if (existingTask.isPresent()) {
+            Task task = existingTask.get();
+            task.setName(updatedTask.getName());
+            task.setDescription(updatedTask.getDescription());
+            task.setCompleted(updatedTask.isCompleted());
+            return taskRepository.save(task);
+
+        }return null;
+    }
+    public void deleteTaskById(Long id){
+        taskRepository.deleteById(id);
+    }
+    public void deleteAllTasks(){
+        taskRepository.deleteAll();
+    }
 }
