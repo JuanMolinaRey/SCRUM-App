5a8ee2f5a86182ff59579e36ef1096ccf4c0b854
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-12 15:37:42 +0200

    feat: add methods for testing task controller

--- src/main/java/com/SCRUM/APP/service/ProjectService.java
+++ src/main/java/com/SCRUM/APP/service/ProjectService.java
@@ -1,4 +1,18 @@
 package com.SCRUM.APP.service;
 
+import com.SCRUM.APP.model.Project;
+import com.SCRUM.APP.repository.IProjectRepository;
+
+import java.util.Optional;
+
 public class ProjectService {
+
+    private final IProjectRepository projectRepository;
+
+    public ProjectService(IProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+    public Optional<Project> getProjectById(Long id) {
+        return projectRepository.findById(id);
+    }
 }
--- src/main/java/com/SCRUM/APP/service/UserService.java
+++ src/main/java/com/SCRUM/APP/service/UserService.java
@@ -1,4 +1,18 @@
 package com.SCRUM.APP.service;
 
+import com.SCRUM.APP.model.User;
+import com.SCRUM.APP.repository.IUserRepository;
+
+import java.util.Optional;
+
 public class UserService {
+
+    private final IUserRepository iuserRepository;
+
+    public UserService(IUserRepository iuserRepository) {
+        this.iuserRepository = iuserRepository;
+    }
+    public Optional<User> getUserById(Long id) {
+        return iuserRepository.findById(id);
+    }
 }
