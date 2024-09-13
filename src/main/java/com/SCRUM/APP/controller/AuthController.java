26f7a38a1ae4fa6a6a307676e5497ae85b3e5727
IsabelGV1985 <isabelmgutierrez@hotmail.es>
2024-09-13 11:59:30 +0200

    first commit Auth

--- src/main/java/com/SCRUM/APP/controller/AuthController.java
+++ src/main/java/com/SCRUM/APP/controller/AuthController.java
@@ -1,4 +1,32 @@
 package com.SCRUM.APP.controller;
 
+import com.SCRUM.APP.dtos.response.AuthResponse;
+import com.SCRUM.APP.dtos.resquest.LoginRequest;
+import com.SCRUM.APP.dtos.resquest.RegisterRequest;
+import com.SCRUM.APP.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+    @PostMapping(value = "login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request)
+    {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping(value = "register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request)
+    {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
 }
--- src/main/java/com/SCRUM/APP/service/AuthService.java
+++ src/main/java/com/SCRUM/APP/service/AuthService.java
@@ -1,4 +1,60 @@
 package com.SCRUM.APP.service;
 
+import com.SCRUM.APP.dtos.response.AuthResponse;
+import com.SCRUM.APP.dtos.resquest.LoginRequest;
+import com.SCRUM.APP.dtos.resquest.RegisterRequest;
+import com.SCRUM.APP.repository.IUserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import com.SCRUM.APP.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class AuthService {
+
+    private final JwtService jwtService;
+    private final IUserRepository iUserRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthService(JwtService jwtService, IUserRepository iUserRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
+        this.jwtService = jwtService;
+        this.iUserRepository = iUserRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+    }
+    public AuthResponse login(LoginRequest login) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(login.getUsername(), login.getPassword()));
+
+        UserDetails user = iUserRepository.findByUsername(login.getUsername()).orElseThrow();
+
+        String token = jwtService.getTokenService(user);
+
+        return AuthResponse
+                .builder()
+                .token(token)
+                .build();
+    }
+
+    public AuthResponse register(RegisterRequest register) {
+        User user = User.builder()
+                .username(register.getUsername())
+                .email(register.getEmail())
+                .password(passwordEncoder.encode(register.getPassword()))
+                .role(register.getRole())
+                .build();
+
+
+        iUserRepository.save(user);
+
+        return AuthResponse
+                .builder()
+                .token(jwtService.getTokenService(user))
+                .role(register.getRole())
+                .build();
+    }
+
 }
