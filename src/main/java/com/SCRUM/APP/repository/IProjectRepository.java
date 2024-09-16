093676552b464206a0a9ba07f5bbc50c0868bbdd
IsabelGV1985 <isabelmgutierrez@hotmail.es>
2024-09-16 14:55:41 +0200

    docker file

--- src/main/java/com/SCRUM/APP/repository/IProjectRepository.java
+++ src/main/java/com/SCRUM/APP/repository/IProjectRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface IProjectRepository extends JpaRepository<Project, Long> {
-    Optional<Project> getProjectByStatus(Status status);
+//    Optional<Project> getProjectByStatus(Status status);
 }
--- src/main/java/com/SCRUM/APP/repository/ITaskRepository.java
+++ src/main/java/com/SCRUM/APP/repository/ITaskRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ITaskRepository extends JpaRepository<Task, Long> {
-    Optional<Task> getTaskByStatus(Status status);
+//    Optional<Task> getTaskByStatus(Status status);
 }
